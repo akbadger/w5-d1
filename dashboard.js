62b58ed57d6766a3fe12d7000c4b2e915e38b361
Aubrey Badger <akbadger@gmail.com>
2017-02-27 13:22:44 -0500

    partial answers to homework questions

--- dashboard.js
+++ dashboard.js
@@ -1,28 +1,70 @@
 // 1. Count the number of records in the "Current Quarter Details" table.
 
+var numberOfRecords = document.querySelector('tr');
+
+numberOfRecords.forEach(function(currentLoopCounter) {
+    var recordCounter = currentLoopCounter + 1;
+});
+
+console.log(recordCounter);
+numberOfRecords = Array.from(numberOfRecords);
+
 
 // 2. Set each dashboard image to different images, one for each season. Make sure you also set the alt="" attribute with an appropriate description.
 
+var images = document.querySelectorAll('#img-responsive');
+
+images.forEach(function(image) {
+    image.src.add('https://unsplash.it/400?random');
+});
 
 // 3. Below each dashboard image, there's a season/quarter name. Below those, there are three dots. Replace those three dots with a randomly generated number formatted as currency, like $12,589.00.
 
 
 // 4. Change the currently selected left side menu item from Overview to Reports.
 
+var activeNavSelection = document.querySelector('#active');
+
+
+
 
 // 5. Set the default search input value to "Q4 sales".
 
+//Added value in HTML, this doesn't work...
+var defaultInputValue = document.querySelector('#navbar-form navbar-right');
+
+defaultInputValue.value.add('Q4 sales');
 
 // 6. Add the "table-hover" class to the table. See the Bootstrap table docs.
+
+var table = document.querySelector('table');
+
+table.classList.add('table-hover');
 
 
 // 7. Add the "success" class to the 7th - 10th row of the table, like: <tr class="success">...</tr>
 
+var tableRow = document.querySelectorAll('tr');
+// Add IDs to each row (with a forEach) and then target rows 7-10 and classList.add
+
 
 // 8. Change the table header names to: ID, First Name, Last Name, Department, Client.
+
+var tableHeaders = document.querySelectorAll('th');
+
+tableHeaders.forEach(function(header) {
+    //add IDs to target each header individually?
+    header.innerText = 'ID', 'First Name', 'Last Name', 'Department', 'Client';
+});
 
 
 // 9. Make an array of objects, each object representing a single row in the table. Remove the commas from the ID column and convert it to an actual Number data type. Example: [{id: 1001, firstName: 'Lorem', lastName: 'ipsum', department: 'dolor', client: 'sit'}]
 
 
 // 10. Make each word in the table capitalized.
+
+var tableData = document.querySelectorAll('.table');
+
+tableData.forEach(function(table) {
+    table.classList.add('text-uppercase');
+});
--- examples/dom.js
+++ examples/dom.js
@@ -1,49 +1,119 @@
+// // Returns an object with the first matching element
+// var button = document.querySelector('.btn');
+
+// button.id = 'myButton';
+// button.title ='Click Here!';
+
+// // if (button.title === 'Click Here!') {
+// //     alert(button.title + ' - The title is amazing.');
+// // }
+
+// //The destructive way to change classes //
+// // button.className = 'btn-success';
+
+// //The better way to change classes //
+// button.classList.add('btn-success');
+
+// button.classList.remove('btn-primary');
+
+// button.classList.toggle('btn-block');
+
+// // if (button.classList.contains('btn-success')) {
+// //     alert('Success!');
+// // }
+
+// // Returns an array of matching elements
+// var buttons = document.querySelectorAll('.btn')
+
+// buttons.forEach(function(button, currentIndex) {
+//     button.classList.add('btn-success');
+//     button.classList.add('btn-block');
+//     button.innerHTML = 'Button ' + (currentIndex + 1);
+
+//     console.log(button.dataset.showWhenLoading);
+// });
+
+// var firstName = document.querySelector('#firstName');
+//     // firstName = firstName.value
+
+//     console.log(firstName);
+
+// var buttons = document.querySelectorAll('.btn');
+
+// buttons.forEach(function(button) {
+//     button.addEventListener('click', showFirstName);
+// });
+
+// function showFirstName() {
+//     document.querySelector('body').innerHTML += firstName.value;
+// }
+
 // Returns an object with the first matching element
 var button = document.querySelector('.btn');
 
+// Manipulate HTML attributes using properties with the same name as the attribute name
 button.id = 'myButton';
-button.title ='Click Here!';
+button.title = 'Click Me!!!!';
 
-// if (button.title === 'Click Here!') {
-//     alert(button.title + ' - The title is amazing.');
+// if (button.title === 'Click Me!!!!') {
+//     alert(button.title + ' - the title is amazing.');
 // }
 
-//The destructive way to change classes //
+
+// The destructive way to change the HTML class attribute
 // button.className = 'btn-success';
 
-//The better way to change classes //
+// The better way to change the HTML class attribute, using the classList object
+
+// Add a new class
 button.classList.add('btn-success');
 
+// Remove an existing class
 button.classList.remove('btn-primary');
 
+// Remove a class if it exists, or add it if it doesn't
 button.classList.toggle('btn-block');
 
-// if (button.classList.contains('btn-success')) {
-//     alert('Success!');
-// }
+// Check to see if a class is already on an element
+if (button.classList.contains('btn-success')) {
+    console.log('The button has a btn-success class on it.');
+}
 
 // Returns an array of matching elements
-var buttons = document.querySelectorAll('.btn')
+var buttons = document.querySelectorAll('.btn');
 
-buttons.forEach(function(button, currentIndex) {
+// You must loop over them to work with each button, one at a time
+// You can add a second argument to a forEach loop to get the current loop counter
+buttons.forEach(function(button, currentLoopCounter) {
+    var buttonCount = currentLoopCounter + 1;
+
     button.classList.add('btn-success');
     button.classList.add('btn-block');
-    button.innerHTML = 'Button ' + (currentIndex + 1);
+    button.innerHTML = 'Button ' + buttonCount;
 
+    // If an HTML element has a Data Attribute, such as data-show-when-loading="yes please"
     console.log(button.dataset.showWhenLoading);
 });
 
+document.querySelector('.col-sm-12').innerHTML += '<ul class="list-group"></ul>'
+
+// Working with input values
 var firstName = document.querySelector('#firstName');
-    // firstName = firstName.value
-
-    console.log(firstName);
-
 var buttons = document.querySelectorAll('.btn');
 
-buttons.forEach(function(button) {
-    button.addEventListener('click', showFirstName);
+document.addEventListener('click', function(e) {
+    var button = e.target;
+
+    if (button.classList.contains('btn')) {
+        appendFirstNameInput();
+    }
 });
 
-function showFirstName() {
-    document.querySelector('body').innerHTML += firstName.value;
+function appendFirstNameInput() {
+    document.querySelector('.list-group').innerHTML += '<li class="list-group-item">' + firstName.value + '</li>';
 }
+
+// querySelectorAll typically gives you a NodeList data type, that only has .length and .forEach()
+// Optionally, Array.from() will convert NodeList to a full Array data type if you need to use .map, .filter, .slice, etc.
+// console.dir(buttons);
+// buttons = Array.from(buttons);
